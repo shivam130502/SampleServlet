fd2a418a7c47385175a8e34d067044c70c7129d2
Shivam Dhir <shivamdhir90@gmail.com>
2024-09-24 17:30:41 +0100

    Resolved Errors : 1. Added Apache Tomcat 10.1.x server, 2. Added servlet info to web.xml deployment descriptor file

--- src/java/FirstServlet.java
+++ src/java/FirstServlet.java
@@ -1,12 +1,13 @@
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
-public class FirstServlet {
+public class FirstServlet extends HttpServlet{
     
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
         
